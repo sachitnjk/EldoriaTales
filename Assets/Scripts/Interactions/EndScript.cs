dd4182fdac6ed943dc88c09a379da597338721a6
Sachit Nanajkar <sachitnjk@gmail.com>
2023-10-29 21:22:52 +0000

    Added end Scene contents, need to add end text

--- Assets/Scripts/Interactions/EndScript.cs
+++ Assets/Scripts/Interactions/EndScript.cs
@@ -1,9 +1,18 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.InputSystem;
 
 public class EndScript : MonoBehaviour
 {
+	private PlayerInput playerInput;
+	private InputAction interactionAction;
+
+	private void Start()
+	{
+		playerInput = InputProvider.GetPlayerInput();
+		interactionAction = playerInput.actions["Interaction"];
+	}
 	private void OnTriggerEnter(Collider other)
 	{
 		UIManager.Instance.interactionBox.text = "Interact to exit Eldoria";
@@ -12,5 +21,14 @@ public class EndScript : MonoBehaviour
 	private void OnTriggerExit(Collider other)
 	{
 		UIManager.Instance.interactionBox.text = null;
+	}
+
+	private void InteractionCheck()
+	{
+		if(interactionAction.triggered) 
+		{
+			Debug.Log("exit called");
+			Application.Quit();
+		}
 	}
 }
