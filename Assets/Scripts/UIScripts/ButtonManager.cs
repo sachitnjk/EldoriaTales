4fd32a901dc5bc81319ce4d61400319557fa00d4
Sachit Nanajkar <sachitnjk@gmail.com>
2023-10-29 01:44:01 +0000

    Adde button manager script for play and exit, settings functionality to be made still

---
+++ Assets/Scripts/UIScripts/ButtonManager.cs
@@ -0,0 +1,19 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class ButtonManager : MonoBehaviour
+{
+
+	public void ChangeToSceneWithIndex(int sceneIndex)
+	{
+		SceneManager.LoadScene(sceneIndex);
+	}
+
+	public void ExitGame()
+	{
+		Application.Quit();
+	}
+
+}
