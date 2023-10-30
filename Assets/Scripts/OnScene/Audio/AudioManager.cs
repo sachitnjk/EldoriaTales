35a19982640fafbef4b84199a6ca4e1af006bd1e
Sachit Nanajkar <sachitnjk@gmail.com>
2023-10-30 11:34:25 +0000

    Added background music, audioManagerScript

---
+++ Assets/Scripts/OnScene/Audio/AudioManager.cs
@@ -0,0 +1,45 @@
+using System.Collections;
+using System.Collections.Generic;
+using Unity.VisualScripting;
+using UnityEngine;
+
+public class AudioManager : MonoBehaviour
+{
+	public static AudioManager Instance;
+	public AudioClip[] backgroundMusicClips;
+	private AudioSource audioSource;
+	private int currentClipIndex = 0;
+
+	private void Awake()
+	{
+		if(Instance == null)
+		{
+			Instance = this;
+			DontDestroyOnLoad(gameObject);
+			audioSource = gameObject.GetComponent<AudioSource>();
+		}
+		else
+		{
+			Destroy(gameObject);
+		}
+
+		PlayNextBGM();
+	}
+
+	private void PlayNextBGM()
+	{
+		if(currentClipIndex < backgroundMusicClips.Length) 
+		{
+			audioSource.clip = backgroundMusicClips[currentClipIndex];
+			audioSource.Play();
+			currentClipIndex++;
+
+			Invoke("PlayNextBGM", audioSource.clip.length);
+		}
+		else
+		{
+			currentClipIndex = 0;
+			PlayNextBGM();
+		}
+	}
+}
